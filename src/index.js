271f3d7459e2e3e8c6e066a741c01719e83b7f2d
PapaMilky <xmadd1@eq.edu.au>
2021-02-27 19:48:43 +1000

    Updated Index.js Added Dynamically Updating Commands

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+const fs = require('fs')
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
+
 client.login(token);
 
-client.on('message', command => {
-    if (!command.content.startsWith(prefix) || command.author.bot) return;
-    if (command.content === `${prefix}ping`) {
-        // send back "Pong." to the channel the message was sent in
-        command.channel.send('Pong');
-    }
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    console.log(command.author, command.content);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+    console.log(message.author, message.content);
 });
 
