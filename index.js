e050f8a605023678c00bb9198a08947205718dc3
PapaMilky <xmadd1@eq.edu.au>
2021-03-04 20:38:19 +1000

    Tinkered With Code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require('./src/config.json');
-const manager = new ShardingManager('./bot.js', { token: `${token}` });
+const manager = new ShardingManager('./src/bot.js', { token: `${token}` });
 
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
