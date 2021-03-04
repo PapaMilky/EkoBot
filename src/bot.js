0227f582fbf6736172228a7b453228ed13e565b9
PapaMilky <xmadd1@eq.edu.au>
2021-03-04 20:50:28 +1000

    Tinkered With **MORE** Code

--- src/bot.js
+++ src/bot.js
@@ -10,7 +10,7 @@ client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./src/commands');
 
 for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
         const command = require(`./commands/${folder}/${file}`);
         client.commands.set(command.name, command);
